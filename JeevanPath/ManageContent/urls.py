7d6e0c318e8418d031dd56198e3aba824fdee96f
rounak-gera <gerarounak890@gmail.com>
2025-07-18 10:16:29 +0530

    uncommented in urls

--- JeevanPath/ManageContent/urls.py
+++ JeevanPath/ManageContent/urls.py
@@ -7,16 +7,16 @@ from .views import (
 )
 
 router = DefaultRouter()
-# router.register(r'home-sections', HomeSectionViewSet)
-# router.register(r'accommodations', AccommodationViewSet)
-# router.register(r'offers', OffersViewSet)
-# router.register(r'gallery', GellaryImagesViewSet)
-# router.register(r'contact', ContactUsViewSet)
-# router.register(r'sliders', SliderImagesViewSet)
-# router.register(r'upcoming-facilities', UpcomingFacilitiesViewSet)
-# router.register(r'about-us', AboutUsViewSet)
-# router.register(r'dining', DiningViewSet)
-# router.register(r'contact-forms', ContactFormViewSet)
+router.register(r'home-sections', HomeSectionViewSet)
+router.register(r'accommodations', AccommodationViewSet)
+router.register(r'offers', OffersViewSet)
+router.register(r'gallery', GellaryImagesViewSet)
+router.register(r'contact', ContactUsViewSet)
+router.register(r'sliders', SliderImagesViewSet)
+router.register(r'upcoming-facilities', UpcomingFacilitiesViewSet)
+router.register(r'about-us', AboutUsViewSet)
+router.register(r'dining', DiningViewSet)
+router.register(r'contact-forms', ContactFormViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
